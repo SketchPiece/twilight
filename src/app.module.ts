fc5590d23e94cf8cacdf558b1f48b217df89d4b8
SketchPiece <lubkin.andrey@gmail.com>
2022-07-02 16:40:00 +0200

    add static serving

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import * as path from 'path'
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: path.join(__dirname, '..', 'client'),
+    }),
+  ],
 })
 export class AppModule {}
