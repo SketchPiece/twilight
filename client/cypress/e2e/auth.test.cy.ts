d380f9e2e0d46eab354671110ced75ebb3b31b48
Alexander Serhiienko <alexanderserhiienko@gmail.com>
2022-07-03 19:25:35 +0300

    beforeEach test

--- client/cypress/e2e/auth.test.cy.ts
+++ client/cypress/e2e/auth.test.cy.ts
@@ -1,6 +1,9 @@
 describe('Specific auth tests', () => {
-  it('Switch between auth methods', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3000/login')
+  })
+
+  it('Switch between auth methods', () => {
     cy.get('[data-cy="switch"]').contains('Register').click()
     cy.url().should('include', '/register')
     cy.get('[data-cy="switch"]').contains('Login').click()
@@ -19,15 +22,17 @@ describe('Specific auth tests', () => {
 })
 
 describe('Login test', () => {
-  it('Without information in inputs', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3000/login')
+  })
+
+  it('Without information in inputs', () => {
     cy.get('[data-cy="submit"]').contains(`Let's go`).click()
     cy.get('[data-cy="nickname-error"]').contains('is a required field')
     cy.get('[data-cy="password-error"]').contains('is a required field')
   })
 
   it('With invalid information in inputs', () => {
-    cy.visit('http://localhost:3000/login')
     cy.get('[data-cy="submit"]').contains(`Let's go`).click()
     cy.get('[data-cy="nickname-input"]').type('123')
     cy.get('[data-cy="password-input"]').type('123456789123456781234')
@@ -40,7 +45,6 @@ describe('Login test', () => {
   })
 
   it('Login only valid nickname entered in input', () => {
-    cy.visit('http://localhost:3000/login')
     cy.get('[data-cy="nickname-input"]').type('1234')
     cy.get('[data-cy="submit"]').contains(`Let's go`).click()
     cy.get('[data-cy="nickname-error"]').should('not.exist')
@@ -48,7 +52,6 @@ describe('Login test', () => {
   })
 
   it('Login only valid password entered in input', () => {
-    cy.visit('http://localhost:3000/login')
     cy.get('[data-cy="password-input"]').type('1234')
     cy.get('[data-cy="submit"]').contains(`Let's go`).click()
     cy.get('[data-cy="nickname-error"]').contains('is a required field')
@@ -56,7 +59,6 @@ describe('Login test', () => {
   })
 
   it('Login with valid information in the inputs', () => {
-    cy.visit('http://localhost:3000/login')
     cy.get('[data-cy="nickname-input"]').type('Guest')
     cy.get('[data-cy="password-input"]').type('1234')
     cy.get('[data-cy="submit"]').contains(`Let's go`).click()
@@ -67,8 +69,11 @@ describe('Login test', () => {
 })
 
 describe('Register test', () => {
-  it('No information in inputs', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3000/register')
+  })
+
+  it('No information in inputs', () => {
     cy.get('[data-cy="submit"]').contains(`I'm excited!`).click()
     cy.get('[data-cy="nickname-error"]').contains('is a required field')
     cy.get('[data-cy="password-error"]').contains('is a required field')
@@ -76,7 +81,6 @@ describe('Register test', () => {
   })
 
   it('With invalid information in inputs', () => {
-    cy.visit('http://localhost:3000/register')
     cy.get('[data-cy="submit"]').contains(`I'm excited!`).click()
     cy.get('[data-cy="nickname-input"]').type('123')
     cy.get('[data-cy="password-input"]').type('123456789123456781234')
@@ -91,7 +95,6 @@ describe('Register test', () => {
   })
 
   it('Only valid nickname entered in input', () => {
-    cy.visit('http://localhost:3000/register')
     cy.get('[data-cy="nickname-input"]').type('1234')
     cy.get('[data-cy="passwordConfirm-input"]').clear()
     cy.get('[data-cy="submit"]').contains(`I'm excited!`).click()
@@ -101,7 +104,6 @@ describe('Register test', () => {
   })
 
   it('Only valid password entered in input', () => {
-    cy.visit('http://localhost:3000/register')
     cy.get('[data-cy="password-input"]').type('1234')
     cy.get('[data-cy="submit"]').contains(`I'm excited!`).click()
     cy.get('[data-cy="nickname-error"]').contains('is a required field')
@@ -110,7 +112,6 @@ describe('Register test', () => {
   })
 
   it('Confirm password not the same as password', () => {
-    cy.visit('http://localhost:3000/register')
     cy.get('[data-cy="password-input"]').type('1234')
     cy.get('[data-cy="passwordConfirm-input"]').type('12345')
     cy.get('[data-cy="submit"]').contains(`I'm excited!`).click()
@@ -118,7 +119,6 @@ describe('Register test', () => {
   })
 
   it('Login with valid information in the inputs', () => {
-    cy.visit('http://localhost:3000/register')
     cy.get('[data-cy="nickname-input"]').type('Guest')
     cy.get('[data-cy="password-input"]').type('1234')
     cy.get('[data-cy="passwordConfirm-input"]').type('1234')
