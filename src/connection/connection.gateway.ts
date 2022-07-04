9f3a1c3fede8406bf29b857277a1e5f95c804075
SketchPiece <lubkin.andrey@gmail.com>
2022-07-04 14:12:09 +0200

    add websockets documentation

--- src/connection/connection.gateway.ts
+++ src/connection/connection.gateway.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger'
 import {
   ConnectedSocket,
   MessageBody,
@@ -7,9 +8,19 @@ import {
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets'
+import { AsyncApiPub, AsyncApiService, AsyncApiSub } from 'nestjs-asyncapi'
 import { Server } from 'socket.io'
 import { AuthSocket } from 'src/common/types'
 
+class MessageDto {
+  @ApiProperty()
+  from: string
+
+  @ApiProperty()
+  text: string
+}
+
+@AsyncApiService()
 @WebSocketGateway()
 export class ConnectionGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
@@ -25,11 +36,36 @@ export class ConnectionGateway implements OnGatewayConnection, OnGatewayDisconne
   }
 
   @SubscribeMessage('message')
+  @AsyncApiPub({
+    channel: 'message',
+    summary: 'Send message to some user',
+    message: {
+      name: 'Test message',
+      payload: {
+        type: MessageDto,
+      },
+    },
+  })
   handleEvent(
     @MessageBody() data: { to: string; text: string },
     @ConnectedSocket() client: AuthSocket
   ) {
     console.log('Message from', client.nickname, 'to id', data.to, ':', data.text)
-    this.server.sockets.to(data.to).emit('message', { from: client.nickname, text: data.text })
+    this.emitMessage(data.to, client.nickname, data.text)
+  }
+
+  @AsyncApiSub({
+    channel: 'message',
+    summary: 'Subscribe to messages',
+    description: 'Method is used for subscribe to messages',
+    message: {
+      name: 'test message',
+      payload: {
+        type: MessageDto,
+      },
+    },
+  })
+  emitMessage(to: string, from: string, text: string) {
+    this.server.sockets.to(to).emit('message', { from: from, text })
   }
 }
