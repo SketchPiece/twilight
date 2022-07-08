c5724ebe8d80efa5609d73d2742461d8babb2143
Alexander Serhiienko <alexanderserhiienko@gmail.com>
2022-07-08 22:38:13 +0300

    fix merge conflict

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './auth'
 
-export const store = configureStore({
+const store = configureStore({
 	reducer: {
 		auth: authReducer,
 	},
@@ -10,3 +10,5 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>
 
 export type AppDispatch = typeof store.dispatch
+
+export default store
