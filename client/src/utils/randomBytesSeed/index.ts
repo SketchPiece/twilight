e17ff66ebb89ea3dc1e2533299de3570a6622713
SketchPiece <lubkin.andrey@gmail.com>
2022-07-09 14:32:23 +0200

    add encryption service

---
+++ client/src/utils/randomBytesSeed/index.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { isaacCSPRNG } = require('./isaacCSPRNG-1.1')
+
+export default function randomBytesSeed(size: number, seed: string): Buffer {
+	const prng = isaacCSPRNG(seed)
+	return Buffer.from(prng.bytes(size))
+}
