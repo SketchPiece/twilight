c7f4a87cd810a8881875304f4d08032c5dfe89af
SketchPiece <lubkin.andrey@gmail.com>
2022-07-09 16:04:48 +0200

    fix randomBytesSeed import for tests

--- client/src/utils/randomBytesSeed/index.ts
+++ client/src/utils/randomBytesSeed/index.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const isaacCSPRNG = require('./isaacCSPRNG-1.1')
+let isaacCSPRNG = require('./isaacCSPRNG-1.1')
+isaacCSPRNG = isaacCSPRNG.default || isaacCSPRNG
 
 export default function randomBytesSeed(size: number, seed: string): Buffer {
 	const prng = isaacCSPRNG(seed)
